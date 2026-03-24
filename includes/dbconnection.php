53f57d4a939919eecff51c90f8db0f105de295d0
Arsalan Khan <arsalanbashirkhan321@gmail.com>
2026-03-24 16:08:50 +0530

    feat: Add initial PDO database connection setup in `dbconnection.php`.

--- includes/dbconnection.php
+++ includes/dbconnection.php
@@ -1,16 +1,15 @@
-<?php 
+<?php
 // DB credentials.
-define('DB_HOST','localhost');
-define('DB_USER','u311638402_tibetan');
-define('DB_PASS','Tibetan@786');
-define('DB_NAME','u311638402_studentmsdb');
+// define('DB_HOST', 'localhost');
+// define('DB_USER', 'root');
+// define('DB_PASS', '');
+// define('DB_NAME', 'tibetan');
 // Establish database connection.
-try
-{
-$dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
-}
-catch (PDOException $e)
-{
-exit("Error: " . $e->getMessage());
+include('config.php');
+
+try {
+    $dbh = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+} catch (PDOException $e) {
+    exit("Error: " . $e->getMessage());
 }
 ?>
